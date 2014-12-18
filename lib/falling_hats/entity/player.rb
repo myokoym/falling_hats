e6f77739bd03dff33524a94aca904f09a8016a65
Masafumi Yokoyama <myokoym@gmail.com>
2014-12-19 02:15:53 +0900

    Split Jewel module

--- lib/falling_hats/entity/player.rb
+++ lib/falling_hats/entity/player.rb
@@ -1,8 +1,11 @@
 require "gosu"
+require "falling_hats/entity/jewel"
 
 module FallingHats
   module Entity
     class Player
+      include Jewel
+
       attr_reader :x, :y, :angle
       def initialize(window, x, y, options={})
         @window = window
@@ -56,40 +59,6 @@ module FallingHats
          @window.button_down?(Gosu::MsRight) or
          @window.button_down?(Gosu::GpRight) or
          @window.button_down?(Gosu::GpButton5)
-      end
-
-      def draw_body
-        draw_upper_body
-        draw_lower_body
-      end
-
-      def draw_upper_body
-        x1 = @x - @radius * 0.5
-        y1 = @y - @radius * 0.7
-        x2 = @x + @radius * 0.5
-        y2 = @y - @radius * 0.7
-        x3 = @x + @radius
-        y3 = @y - @radius * 0.1
-        x4 = @x - @radius
-        y4 = @y - @radius * 0.1
-        @window.draw_quad(x1, y1, @color,
-                          x2, y2, @color,
-                          x3, y3, @color,
-                          x4, y4, @color,
-                          @z_order)
-      end
-
-      def draw_lower_body
-        x1 = @x - @radius
-        y1 = @y
-        x2 = @x + @radius
-        y2 = @y
-        x3 = @x
-        y3 = @y + @radius
-        @window.draw_triangle(x1, y1, @color,
-                              x2, y2, @color,
-                              x3, y3, @color,
-                              @z_order)
       end
     end
   end
